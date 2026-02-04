fe4650a7e7772ae124509a465bbefbb36cb877a7
alvarofernandezmota-tech <alvarofernandezmota@gmail.com>
2026-02-04 20:17:31 +0100

    feat: Crear carpeta funciones con __init__.py

---
+++ proyectos/thdora-bot/funciones/__init__.py
@@ -0,0 +1,11 @@
+"""
+Módulo de funciones de THDORA
+Contiene todas las funcionalidades del bot de agenda
+"""
+
+# Aquí irán los imports de las funciones cuando las creemos
+# from .agregar_cita import agregar_cita
+# from .mostrar_citas import mostrar_citas_dia, mostrar_citas_mes
+# from .eliminar_cita import eliminar_cita
+# from .buscar_cita import buscar_cita
+# from .guardar_datos import cargar_datos, guardar_datos
